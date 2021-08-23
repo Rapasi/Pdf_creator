8d3a858fade4365c32e5d13d2957e950eb14925f
Rami Erkkilä <ramierk@gmail.com>
2021-08-23 19:37:28 +0300

    Split to two files

---
+++ create_pdf.py
@@ -0,0 +1,31 @@
+#Creating pdf using fpdf
+
+from fpdf import FPDF
+from datetime import datetime
+
+# Setting start date and end date for graphs  
+
+start=datetime(2019,1,1)
+end=datetime.today()
+# Dimensions of document (not accurate currently)
+
+WIDTH=210
+HEIGHT=590
+
+pdf=FPDF()
+pdf.add_page()
+pdf.set_font('Arial','B',16)
+pdf.set_fill_color(155,50,80)
+pdf.cell(80,10,f'Osakekurssit {start.strftime("%m/%d/%y").replace("/0","/").lstrip("0")}-{end.strftime("%m/%d/%y").replace("/0","/").lstrip("0")}',fill=True)
+pdf.image('Kuvaaja_0.png',5,30,WIDTH/2-5)
+pdf.image('Kuvaaja_1.png',WIDTH/2+5,30,WIDTH/2-5)
+pdf.image('kuvaaja_2.png',5,115,WIDTH/2-5)
+pdf.image('kuvaaja_3.png',WIDTH/2+5,115,WIDTH/2-5)
+
+pdf.add_page()
+pdf.image('kuvaaja_4.png',5,30,WIDTH/2-5)
+pdf.image('kuvaaja_5.png',WIDTH/2+5,30,WIDTH/2-5)
+pdf.image('kuvaaja_6.png',5,110,WIDTH/2-5)
+pdf.image('kuvaaja_7.png',WIDTH/2+5,110,WIDTH/2-5)
+pdf.image('kuvaaja_8.png',5,190,WIDTH/2-5)
+pdf.output(r'C:\Users\ramie\Projects\report_pdf','F')
